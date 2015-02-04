380c95e07516d33692adaf1856f261783141923b
sujata-patne <sujata.halwai@synerzip.com>
2015-02-04 18:40:27 +0530

    Modified Organizations for update organization, add/delete projects

--- app/scripts/controllers/organizationsedit.js
+++ app/scripts/controllers/organizationsedit.js
@@ -8,11 +8,36 @@
  * Controller of the dashboardApp
  */
 angular.module('dashboardApp')
-  .controller('OrganizationseditCtrl',['$scope', '$stateParams','OrganizationsService', function ($scope, $stateParams, OrganizationsService) {
+  .controller('OrganizationseditCtrl',['$scope', '$state', '$stateParams','OrganizationsService', function ($scope, $state, $stateParams, OrganizationsService) {
 
     $scope.organization = OrganizationsService.getOrganization($stateParams.id);
-
-    $scope.getProjectsArr = function(id){
-      return OrganizationsService.getProjectsArr(id);
+    //get last projectID
+    $scope.projectIndex = $scope.organization.projects.length+1;
+    /*Add New Project Field*/
+    $scope.newProject = { name: [] };
+    $scope.addProject = function() {
+      $scope.newProject.name.push('');
     }
+
+    $scope.deleteProjectField = function(index) {
+      $scope.newProject.name.splice(index, 1);
+    }
+
+    $scope.deleteProject = function(id) {
+      angular.forEach($scope.organization.projects, function(item,key){
+        if(item.id == id){
+          $scope.organization.projects.splice(key,1);
+        }
+      })
+    }
+
+    $scope.update=function(){
+      angular.forEach($scope.newProject.name, function(item){
+        var newProjectName = item;
+        var newProjectId = ++$scope.projectIndex;
+        $scope.organization.projects.push({"id":newProjectId,"name":newProjectName});
+      })
+      OrganizationsService.update($scope.organization);
+      $state.transitionTo('organizations.view',{"id" : $scope.organization.id});
+    };
   }]);
